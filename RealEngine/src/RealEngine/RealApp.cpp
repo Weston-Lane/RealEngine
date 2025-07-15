d2c59a3bdff90a8ff3a42384db7c20c202e27406
Weston Lane <weston.r.lane@gmail.com>
2025-07-15 01:56:17 -0400

    GLFW commit

--- RealEngine/src/RealEngine/RealApp.cpp
+++ RealEngine/src/RealEngine/RealApp.cpp
@@ -1,6 +1,8 @@
+#include "pch.h"
 #include "RealApp.h"
 #include "Logger.h"
 #include "Events/ApplicationEvent.h"
+#include "GLFW/glfw3.h"
 namespace Real
 {
 	RealApp::RealApp()
@@ -13,6 +15,7 @@ namespace Real
 	}
 	void RealApp::Run()
 	{
+		RL_CORE_DEBUG("EVENT SYSTEM CHECK:")
 		WindowCloseEvent event;
 		RL_CORE_TRACE(" {}",event.ToString());
 		EventDispatcher dispatcher(event);
